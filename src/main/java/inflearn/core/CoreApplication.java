f17161d31a3f8906620ee41482ecf42573607c91
acisliver <7273322@naver.com>
2022-05-21 06:01:47 +0900

    회원 도메인 개발

--- src/main/java/inflearn/core/CoreApplication.java
+++ src/main/java/inflearn/core/CoreApplication.java
@@ -3,10 +3,13 @@ package inflearn.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
+		Collection
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
