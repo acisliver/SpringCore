4d7e242866fd8a9ede7f9ea01abbfba7a123177b
acisliver <7273322@naver.com>
2022-05-31 19:13:55 +0900

    컴포넌트 스캔 탐색 위치

--- src/main/java/inflearn/core/AutoAppConfig.java
+++ src/main/java/inflearn/core/AutoAppConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+// 컴포넌트 default 스캔 시작 위치는 @ComponentScan의
+// 프로젝트 루트에 @ComponentScan을 붙이고 하위 클래스를 스캔
 @Configuration
 @ComponentScan(
+        basePackages = "inflearn.core",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
