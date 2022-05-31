f71e9a79f6224777d135d0cc1f63f562fb26dcc5
acisliver <7273322@naver.com>
2022-05-31 10:05:56 +0900

    @Configuration과 바이트코드 조작

--- src/test/java/inflearn/core/singelton/ConfigurationSingletonTest.java
+++ src/test/java/inflearn/core/singelton/ConfigurationSingletonTest.java
@@ -30,4 +30,18 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // 순수 클래스라면 class inflearn.core.AppConfig
+        // 하지만 내가 만든 순수 클래스가 아니다
+        // 바이트코드 조작 라이브러리를 사용해 다른 클래스를 빈으로 등록
+        // CGLIB으로 조작된 코드는 싱글턴이 보장될 것이다
+        // @Configuration을 사용하지 않으면 빈은 등록이 되지만 싱글턴을 유지하지 못한다.
+        // DI도 되지 않고 new를 계속 한다.
+        System.out.println("bean = " + bean.getClass());
+    }
 }
