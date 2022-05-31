19e17ecab8aeb853a828031a5130b90d7d84447b
acisliver <7273322@naver.com>
2022-05-31 09:53:20 +0900

    @Configuration이 싱글톤을 보장해준다

--- src/test/java/inflearn/core/singelton/StatefulServiceTest.java
+++ src/test/java/inflearn/core/singelton/StatefulServiceTest.java
@@ -2,7 +2,6 @@ package inflearn.core.singelton;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.batch.BatchDataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
