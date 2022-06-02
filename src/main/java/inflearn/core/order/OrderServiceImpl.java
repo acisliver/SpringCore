8055152ad5b609eae0520792548bfbfa37cb4901
acisliver <7273322@naver.com>
2022-06-02 23:40:23 +0900

    필드 주입 - 테스트 작성시 필드로 바로 주입이 되어 목 객체를 사용할 수 없다

--- src/main/java/inflearn/core/order/OrderServiceImpl.java
+++ src/main/java/inflearn/core/order/OrderServiceImpl.java
@@ -8,19 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스뿐만 아니라 구현 클래스도 의존하고 있다. -> DIP, OCP 위반
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    // 세터 주입
-    @Autowired(required = false)
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    }
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
     // 생성자가 하나라면 @Autowired가 없어도 final로 선언된 빈을 등록해준다
 //    @Autowired
