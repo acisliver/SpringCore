27eb217d135fef39babac2cf67148f3f0e1ba9fb
acisliver <7273322@naver.com>
2022-06-03 14:52:54 +0900

    테스트 수정

--- src/test/java/inflearn/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/inflearn/core/discount/RateDiscountPolicyTest.java
@@ -1,15 +1,20 @@
 package inflearn.core.discount;
 
+import inflearn.core.AppConfig;
 import inflearn.core.member.Grade;
 import inflearn.core.member.Member;
+import inflearn.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RateDiscountPolicyTest {
 
-    RateDiscountPolicy discountPolicy;
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    RateDiscountPolicy discountPolicy = ac.getBean("discountPolicy", RateDiscountPolicy.class);
 
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다")
--- src/test/java/inflearn/core/member/MemberServiceTest.java
+++ src/test/java/inflearn/core/member/MemberServiceTest.java
@@ -2,12 +2,15 @@ package inflearn.core.member;
 
 import inflearn.core.AppConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
 
-    MemberService service;
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService service = ac.getBean("memberService", MemberService.class);
 
     @Test
     void join() {
