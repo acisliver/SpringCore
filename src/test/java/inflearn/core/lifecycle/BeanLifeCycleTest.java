23cf00f3cf2b89a91f91d01d60108e7d6e4da8dc
acisliver <7273322@naver.com>
2022-06-07 15:28:49 +0900

    빈 생명주기 - 객체 생성 후 의존관계 주입(세터, 필드 인젝션)이 모두 다 된 상태에서 데이터를 사용할 수 있다. -> 초기화 시점을 콜백을 통해 알려준다.

---
+++ src/test/java/inflearn/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package inflearn.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); // 보통 close할 일이 없기 때문에 이 메서드는 기본 ApplicationContext에는 정의되어있지 않다
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://inflearn-core.dev");
+            return networkClient;
+        }
+    }
+}
