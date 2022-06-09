02ffbdc666c96f99f2a7432d7bc4601321ee050b
acisliver <7273322@naver.com>
2022-06-09 22:31:25 +0900

    Provider 적용

--- src/main/java/inflearn/core/web/LogDemoService.java
+++ src/main/java/inflearn/core/web/LogDemoService.java
@@ -2,15 +2,17 @@ package inflearn.core.web;
 
 import inflearn.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger logger;
+    private final ObjectProvider<MyLogger> provider;
 
     public void logic(String testId) {
+        MyLogger logger = provider.getObject();
         logger.log("service id = " + testId);
     }
 }
